eb46f9923448828e815844d4b27397426995e442
ghufr <ghufronfr@gmail.com>
2020-03-20 11:13:21 +0700

    scraper

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const { Builder } = require("selenium-webdriver");
+require("dotenv").config();
+const { Builder, By } = require("selenium-webdriver");
+const baseUrl = "https://www.hackerrank.com";
+// const contestName = "jurnal-online-alpro-2020";
 
+// UNFINISHED, USE REST API INSTEAD
 (async () => {
   const driver = await new Builder().forBrowser("firefox").build();
   try {
-    // Do something
+    await driver.get(`${baseUrl}/auth/login`);
+    await driver
+      .findElement(By.id("input-1"))
+      .sendKeys(process.env.HR_USERNAME);
+    await driver
+      .findElement(By.id("input-2"))
+      .sendKeys(process.env.HR_PASSWORD);
+    await (
+      await driver.findElement(
+        By.xpath("//button[@data-analytics='LoginPassword']")
+      )
+    ).click();
   } catch (err) {
-    console.err(err);
+    console.log(err);
     await driver.quit();
   }
 })();
